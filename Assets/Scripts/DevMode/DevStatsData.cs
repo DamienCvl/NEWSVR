c5fd141889e1d0e0e82995448eff71ad66f9cab9
Damien Chevalerias <damien.chevalerias@gmail.com>
2019-08-12 11:27:01 +0200

    add row prefab and DevStatsData class
    row prefab : row to display data
    DevStatsData : class to formatting data

---
+++ Assets/Scripts/DevMode/DevStatsData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.DevMode
+{
+    class DevStatsData
+    {
+        private string playerName;
+        private string newsTitle;
+        private string reaction;
+        private uint nbCmt;
+        private DateTime date;
+
+        public DevStatsData(string playerName, string newsTitle, uint reaction, uint nbCmt, DateTime date)
+        {
+            this.playerName = playerName;
+            this.newsTitle = newsTitle;
+            this.reaction = GetStringReaction(reaction);
+            this.nbCmt = nbCmt;
+            this.date = date;
+        }
+
+        private string GetStringReaction(uint reaction)
+        {
+            switch (reaction)
+            {
+                case 0:
+                    return "None";
+                case 1:
+                    return "Happy";
+                case 2:
+                    return "Sad";
+                case 3:
+                    return "Angry";
+                case 4:
+                    return "Surprised";
+                default:
+                    return "Unknown";
+            }
+        }
+    }
+}
