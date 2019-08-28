dad1045d7839cd28035f561e53eea174d9e07c43
Damien Chevalerias <damien.chevalerias@gmail.com>
2019-08-28 15:32:08 +0200

    fix

--- Assets/Scripts/TownSimulation/NewsGO/Reaction/SurpriseNbrView.cs
+++ Assets/Scripts/TownSimulation/NewsGO/Reaction/SurpriseNbrView.cs
@@ -1,10 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using Mono.Data.Sqlite;
-using System.Data;
-using System;
 using Assets.Scripts.Core;
 
 namespace Assets.Scripts.TownSimulation.NewsGO.Reaction
